242d923684c002154acb72edb532dba1a107db00
PankajAjmera1 <coderajmera@gmail.com>
2024-07-13 10:43:46 +0530

    shortkar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -41,12 +41,12 @@ function Home() {
       <Accordion type="multiple" collapsible className="w-full md:px-11">
         <AccordionItem value="item-1">
           <AccordionTrigger>
-            How does the Trimrr URL shortener works?
+            How does the ShortKar - URL shortener works?
           </AccordionTrigger>
           <AccordionContent>
             When you enter a long URL, our system generates a shorter version of
             that URL. This shortened URL redirects to the original long URL when
-            accessed.
+            accessed.Also Generate a QR code for the short URL.
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-2">
